b8e116d063d94a16a8f3447462e42bbc921c1a4c
Mudit Madhwal <muditmadhwal@gmail.com>
2023-08-01 09:44:54 +0530

    End of section 3

---
+++ client/src/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Ritik");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Satyasish");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Anuj");
