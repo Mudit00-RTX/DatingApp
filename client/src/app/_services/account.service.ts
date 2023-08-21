b1d5078ac4fa66b477e5cabefd921ffcb8673e21
Mudit Madhwal <muditmadhwal@gmail.com>
2023-08-21 10:55:36 +0530

    End of section 9

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,17 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { User } from '../_models/user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-baseURL = 'https://localhost:5001/api/';
+baseUrl = environment.apiUrl;
 private currentUserSource = new BehaviorSubject<User | null>(null);
 currentUser$ = this.currentUserSource.asObservable();
   constructor(private http: HttpClient) { }
   login(model:any){
-    return this.http.post<User>(this.baseURL + 'account/login',model).pipe(
+    return this.http.post<User>(this.baseUrl + 'account/login',model).pipe(
       map((response: User)=>{
         const user = response;
         if(user){
@@ -23,7 +24,7 @@ currentUser$ = this.currentUserSource.asObservable();
     )
   }
   register(model: any){
-    return this.http.post<User>(this.baseURL + 'account/register', model).pipe(
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
       map(user =>{
         if(user){
           localStorage.setItem('user',JSON.stringify(user));
